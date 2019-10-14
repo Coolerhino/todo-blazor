9bccc3fbf569a7589312974f6ed5f8bf28b996a9
Lato, Emilian {FISG~Warsaw} <latoe@emea.roche.com>
2019-10-14 08:59:08 +0200

    Add controller Update package

---
+++ TodoBlazor.Server/Controllers/TodoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoBlazor.Core.Models;
+using TodoBlazor.Data;
+
+namespace TodoBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<TodoItem>> GetItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<TodoItem> CreateToDoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+
+        [HttpPut]
+        public async Task<TodoItem> UpdateToDoItem(TodoItem item)
+        {
+            _context.TodoItems.Update(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+    }
+}
--- TodoBlazor.Server/Startup.cs
+++ TodoBlazor.Server/Startup.cs
@@ -18,7 +18,6 @@ namespace TodoBlazor.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddDbContext<TodoContext>(
                 options => options.UseSqlServer(
                     "Server=(localdb)\\mssqllocaldb;Database=TodoDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
